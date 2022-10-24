165e2aa471378c594ff65083bce0cfedbe5ea17a
Brooke Vonderheid <bevfrisco@gmail.com>
2022-10-24 16:33:47 -0500

    created models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class VehicleVO(models.Model):
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+    name = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100, unique=True)
+
+
+class Technician(models.Model):
+    name = models.CharField(max=200)
+    employee_num = models.PositiveSmallIntegerField()
+
+
+class Appointment(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    owner = models.CharField(max_length=100)
+    scheduled_time = models.DateTimeField(default=timezone.now)
+    reason = models.CharField(max_length=200)
+    vip = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default = False)
+    technician = models.ForeignKey(Technician,
+     related_name="technician",
+      on_delete=models.CASCADE)
+
+
