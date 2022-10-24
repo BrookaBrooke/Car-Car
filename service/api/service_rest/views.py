63fbd55aabf16cbb03f71b0d444a14661e71c6c7
Brooke Vonderheid <bevfrisco@gmail.com>
2022-10-24 17:04:04 -0500

    added vo encoder

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from common.json import ModelEncoder
+from django.views.decorators import requirehttpmethods
+import json
+from django.http import JsonResponse
+from .models import VehicleVO, Appointment, Technician
 # Create your views here.
+
+
+class VehicleVOEncoder():
+    model = VehicleVO
+    properties = [
+        "color",
+        "year",
+        "vin",
+        "name",
+        "manufacturer",
+    ]
+
+class AppointmentEncoder():
+    model = Appointment
+    properties = [
+        "vin",
+        "owner",
+        "scheduled_time",
+        "vip",
+
+    ]
+
+class AppointmentDetailEncoder():
+    pass
+
