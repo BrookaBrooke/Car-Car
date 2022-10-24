c2eb0ad80de8a8084015c595b65f22c100ff3025
Brooke Vonderheid <bevfrisco@gmail.com>
2022-10-24 16:39:59 -0500

    registered in admin

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import VehicleVO, Technician, Appointment
 # Register your models here.
+class VehicleVOAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(VehicleVO, VehicleVOAdmin)
+class TechnicianAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Technician, TechnicianAdmin)
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Appointment, AppointmentAdmin)
