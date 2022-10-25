99bda58d9a0d4c3befb6c887551807b9e2628bd9
Brooke Vonderheid <bevfrisco@gmail.com>
2022-10-25 16:52:27 -0500

    added constructor method

--- ghi/app/src/AutomobileService/AddAppointment.js
+++ ghi/app/src/AutomobileService/AddAppointment.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 class AddAppointment extends React.Component {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            vin: [],
+            owner: [],
+            scheduled_time: [],
+            reason: [],
+            vip: [],
+            is_completed: [],
+            technician: [],
+        };
+    }
 }
