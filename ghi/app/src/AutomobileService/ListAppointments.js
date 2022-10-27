fc9062347206c03b356c85272dba92c0316cbc80
Brooke Vonderheid <bevfrisco@gmail.com>
2022-10-27 00:46:28 -0500

    added services dropdown

---
+++ ghi/app/src/AutomobileService/ListAppointments.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+class ListAppointments extends React.Component{
+
+}
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -8,7 +8,21 @@ function Nav() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+        <div className="nav-item dropdown">
+        <div className='nav-link dropdown toggle' role="button" id="navbarDropDownMenuLink" data-toggle="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    Automobile Services
+                  </div>
+                  <div className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropDownMenuLink">
+                  <li className="dropdown-item">
+                            <NavLink className="nav-link" aria-current="page" to="/technician/new/">Add Technician</NavLink>
+                        </li>
+                        <li className="dropdown-item">
+                            <NavLink className="nav-link" aria-current="page" to="/appointment/new/">Add Appointment</NavLink>
+                        </li>
+                        </div>
+              </div>
                 <div className="nav-item dropdown">
+
                     <div className="nav-link dropdown-toggle" role="button" id="navbarDropDownMenuLink" data-toggle="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
                         Automobile Sales
                     </div>
@@ -22,12 +36,7 @@ function Nav() {
                         <li className="dropdown-item">
                             <NavLink className="nav-link" aria-current="page" to="/customers/new/">Add Customer</NavLink>
                         </li>
-                        <li className="dropdown-item">
-                            <NavLink className="nav-link" aria-current="page" to="/technician/new/">Add Technician</NavLink>
-                        </li>
-                        <li className="dropdown-item">
-                            <NavLink className="nav-link" aria-current="page" to="/appointment/new/">Add Appointment</NavLink>
-                        </li>
+
                     </div>
                 </div>
           </div>
